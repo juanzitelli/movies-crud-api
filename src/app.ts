968a03c06d29be0f3e58b2a700a1b97cf1f5942d
Juan Zitelli <juanzitelli7@gmail.com>
2021-10-08 03:30:36 -0300

    feat(app.ts, server.ts): update both app and server files
    This commits add the final version of the app and server files to allow the API to run in the
    desired port, connect to the database and log errors if any.

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,11 @@
 import express from "express";
+import dotenv from "dotenv";
+import { mainRouter, movieRouter } from "./routers";
+import cors from "cors";
 
 export const app = express();
+dotenv.config();
 
-app.use("/", (req, res) => {
-  res.json({ status: "ok 👍🏻" });
-});
+app.use(express.json());
+app.use(cors());
+app.use(mainRouter, movieRouter);
