354f8a9facd3d99f8eb557a3680c4da05c60cb76
Juan Agustín Zitelli <juanzitelli7@gmail.com>
2021-10-11 16:05:19 -0300

    style: extra spacing

--- src/app.ts
+++ src/app.ts
@@ -5,10 +5,11 @@ import cors from "cors";
 
 export const app = express();
 dotenv.config();
+
 // bodyParser constructor was deprecated, so I rather use this than:
 //app.use(bodyParser.urlencoded());
 //app.use(bodyParser.json());
-
 app.use(express.json());
+
 app.use(cors());
 app.use(mainRouter, movieRouter);
